0bda9c4e0b063439b601a7d0e05544563b229c0d
juneKim <migkjy@naver.com>
2018-01-25 17:10:59 -0800

    resetted project

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Blaze from 'meteor/gadicc:blaze-react-component';
 import { withTracker } from 'meteor/react-meteor-data';
 
-import { Form, FormGroup, Col, FormControl, ControlLabel, Checkbox, Button } from 'react-bootstrap';
-
 import { RealEstates } from './../api/real-estates';
 // import { reLists } from './../fixtures/fixtures';
 import RealEstate from './RealEstate';
@@ -24,27 +22,10 @@ class App extends React.Component {
         <div>
           <Blaze template="loginButtons" />
         </div>
+        <p>Hello!!</p>
         <ul>
           {this.renderRealEstatesList()}
         </ul>
-        <p>Hello!!</p>
-        <div>
-          <Form horizontal onSubmit={() => {}}>
-            <FormGroup>
-              <Col componentClass={ControlLabel} sm={2}>
-                New Asset
-              </Col>
-              <Col sm={6}>
-                <FormControl type="text" placeholder="Type to add new Asset" />
-              </Col>
-              <Col sm={2}>
-                <Button type="submit">
-                  Sign in
-                </Button>
-              </Col>
-            </FormGroup>
-          </Form>
-        </div>
       </div>
     );
   }
